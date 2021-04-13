c0553fab43f937ebddd62858a6516a1b15d4b454
ChristianAnagnostou <69920522+ChristianAnagnostou@users.noreply.github.com>
2021-04-13 16:09:43 -0700

    update config

--- config/index.js
+++ config/index.js
@@ -1,3 +1,1 @@
-const dev = process.env.NODE_ENV !== "production";
-
-export const API_URL = dev ? "http://localhost:3000" : "https://your_deployment.server.com";
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
