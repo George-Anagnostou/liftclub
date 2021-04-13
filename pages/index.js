a820bb92eaa7f1c9f707367c0791eaa1f0263066
ChristianAnagnostou <69920522+ChristianAnagnostou@users.noreply.github.com>
2021-04-13 14:23:20 -0700

    state for invalid credentials

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,63 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { useState } from "react";
+import Layout from "../components/Layout";
+import { useStoreContext } from "../context/state";
 
-export default function Home() {
+const Home = () => {
+  const { authUser, user } = useStoreContext();
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [invalidCreds, setInvalidCreds] = useState(true);
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    const userData = await authUser(username, password);
+    
+    userData
+      ? (localStorage.setItem("workoutID", userData._id), setInvalidCreds(true))
+      : setInvalidCreds(false);
+  };
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <Layout>
+      <div>
+        {!user && (
+          <form action="post" onSubmit={handleLogin}>
+            <label htmlFor="username">Login username: </label>
+            <input
+              type="text"
+              name="username"
+              id="username"
+              value={username}
+              onChange={handleUsernameChange}
+            />
+            <label htmlFor="password">Login password: </label>
+            <input
+              type="text"
+              name="password"
+              id="password"
+              value={password}
+              onChange={handlePasswordChange}
+            />
+            <button type="submit" onClick={handleLogin}>
+              Login
+            </button>
+          </form>
+        )}
+        {!invalidCreds && <p>Email or Password was incorrect</p>}
+        {user && <h1>Hello {user.username}</h1>}
+      </div>
+    </Layout>
+  );
+};
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
-}
+export default Home;
