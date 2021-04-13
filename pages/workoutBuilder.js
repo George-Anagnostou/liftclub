fe46007342dab566ef444eee37ac24c492b840ce
ChristianAnagnostou <69920522+ChristianAnagnostou@users.noreply.github.com>
2021-04-13 14:24:04 -0700

    workoutBuilder and workoutLog page

---
+++ pages/workoutBuilder.js
@@ -0,0 +1,526 @@
+import { useState, useEffect } from "react";
+import styled from "styled-components";
+
+import { API_URL } from "../config";
+import Layout from "../components/Layout";
+import { useStoreContext } from "../context/state";
+import {
+  getExercisesFromIdArray,
+  getUserMadeWorkouts,
+  postNewWorkout,
+  updateExistingWorkout,
+} from "../utils/ApiSupply";
+
+const muscleGroups = [
+  "all",
+  "upper back",
+  "lower back",
+  "shoulder",
+  "upper arm",
+  "forearm",
+  "chest",
+  "hip",
+  "upper leg",
+  "lower leg",
+  "core",
+];
+
+const motions = [
+  "all",
+  "pulling",
+  "pushing",
+  "thrusting",
+  "curling",
+  "squatting",
+  "rotating",
+  "crunching",
+  "breathing",
+];
+
+export default function workoutBuilder({ exercises }) {
+  const { user } = useStoreContext();
+
+  const [workoutSavedSuccessfuly, setWorkoutSavedSuccessfuly] = useState(null);
+  const [userWorkouts, setUserWorkouts] = useState([]);
+  const [displayedExercises, setDisplayedExercises] = useState(exercises);
+  const [customWorkoutExercises, setCustomWorkoutExercises] = useState([]);
+  const [customWorkoutName, setCustomWorkoutName] = useState("New Workout");
+
+  const loadUserMadeWorkouts = async () => {
+    const userMadeWorkouts = await getUserMadeWorkouts(user._id);
+    setUserWorkouts(userMadeWorkouts);
+  };
+
+  // Returns boolean for whether or not an exercise exists in customWorkoutExercises
+  const isExerciseInCustomWorkout = (exercise_id) => {
+    return customWorkoutExercises.map((item) => item.exercise_id).includes(exercise_id);
+  };
+
+  // Adds NEW exercises to customWorkoutExercises
+  const addExercise = (exercise) => {
+    if (!isExerciseInCustomWorkout(exercise._id)) {
+      setCustomWorkoutExercises((prev) => [
+        ...prev,
+        { exercise: exercise, exercise_id: exercise._id, sets: [] },
+      ]);
+    }
+  };
+
+  // Removes an exercise from customWorkoutExercises
+  const removeExercise = (exercise) => {
+    const filteredArr = customWorkoutExercises.filter((each) => each.exercise_id !== exercise._id);
+    setCustomWorkoutExercises(filteredArr);
+  };
+
+  // Resets custom workout state
+  const clearCustomWorkout = () => {
+    setCustomWorkoutExercises([]);
+    setCustomWorkoutName("New Workout");
+  };
+
+  // Handles changes for customWorkoutName
+  const handleWorkoutNameChange = (e) => {
+    setCustomWorkoutName(e.target.value);
+  };
+
+  //
+  const handleRepChange = (e, exercise_id, setIndex) => {
+    const num = Number(e.target.value);
+    // Copy state
+    const customExercisesCopy = [...customWorkoutExercises];
+    // Update the reps for specified set
+    customExercisesCopy.map((item) => {
+      if (item.exercise_id === exercise_id) {
+        item.sets[setIndex].reps = num || "";
+      }
+    });
+
+    setCustomWorkoutExercises(customExercisesCopy);
+  };
+
+  const changeSetLength = (method, exerciseIndex) => {
+    // Copy  state
+    const customExercisesCopy = [...customWorkoutExercises];
+
+    switch (method) {
+      case "add":
+        // Add empty set to spedified exercise
+        customExercisesCopy[exerciseIndex].sets.push({ reps: 0, weight: 0, weightUnit: "lbs" });
+        break;
+      case "remove":
+        // Remove last set from spedified exercise
+        customExercisesCopy[exerciseIndex].sets.pop();
+        break;
+    }
+
+    setCustomWorkoutExercises(customExercisesCopy);
+  };
+
+  const saveWorkoutToDB = async () => {
+    // Only take exercise_id and sets (exercise data not needed for DB)
+    const workoutForDB = customWorkoutExercises.map(({ exercise_id, sets }) => {
+      return { exercise_id, sets };
+    });
+
+    // Search for workout by name if it already exists
+    const existingWorkout = userWorkouts.find((workout) => workout.name === customWorkoutName);
+
+    if (existingWorkout) {
+      // Put exercises in correct format (without exercise data)
+      existingWorkout.exercises = workoutForDB;
+
+      try {
+        const res = await updateExistingWorkout(existingWorkout);
+        setWorkoutSavedSuccessfuly(res.status === 204);
+      } catch (e) {
+        console.log(e);
+      }
+    } else {
+      // Create workout obj to send to DB
+      const composedWorkout = {
+        name: customWorkoutName,
+        creator_id: user._id,
+        exercises: workoutForDB,
+        public: false,
+      };
+
+      try {
+        const res = await postNewWorkout(composedWorkout);
+        setWorkoutSavedSuccessfuly(res.status === 201);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+
+    clearCustomWorkout();
+    loadUserMadeWorkouts();
+  };
+
+  const filterExercisesBy = async ({ field, value }) => {
+    try {
+      let res;
+      // Don't use "field" and "value" for searching by "all"
+      value === "all"
+        ? (res = await fetch(`/api/exercises`))
+        : (res = await fetch(`/api/exercises?${field}=${value}`));
+
+      const queried = await res.json();
+      setDisplayedExercises(queried);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const displaySavedWorkout = async (workout) => {
+    // Grab all the exercise_ids from the workout
+    const idArr = workout.exercises.map((each) => each.exercise_id);
+    // Query for exercise data using the idArr
+    const exerciseData = await getExercisesFromIdArray(idArr);
+
+    // Create exercise key in each exercise to hold exercise data
+    workout.exercises.map((each, i) => {
+      // Ensure ids match
+      if (each.exercise_id === exerciseData[i]._id) {
+        each.exercise = exerciseData[i];
+      }
+      return each;
+    });
+
+    setCustomWorkoutExercises(workout.exercises);
+    setCustomWorkoutName(workout.name);
+  };
+
+  // Remove saved successfully notification after 5 seconds
+  useEffect(() => {
+    if (workoutSavedSuccessfuly) {
+      setTimeout(() => {
+        setWorkoutSavedSuccessfuly(null);
+      }, 5000);
+    }
+  }, [workoutSavedSuccessfuly]);
+
+  // Call loadUserMadeWorkouts if user is logged in
+  useEffect(() => {
+    if (user) {
+      loadUserMadeWorkouts();
+    }
+  }, [user]);
+
+  return (
+    <Layout>
+      <Container>
+        <CustomWorkout>
+          <div className="workout-control">
+            <div className="workout-header">
+              <h3>Custom Workout</h3>
+              {workoutSavedSuccessfuly && (
+                <p style={{ color: "green" }}>Workout saved successfully</p>
+              )}
+              {Boolean(customWorkoutExercises.length) && (
+                <>
+                  <button onClick={saveWorkoutToDB}>Save</button>
+                  <button onClick={clearCustomWorkout}>Clear</button>
+                </>
+              )}
+            </div>
+
+            <label htmlFor="workoutName">Workout Name:</label>
+            <input
+              type="text"
+              name="workoutName"
+              id="workoutName"
+              value={customWorkoutName}
+              onChange={handleWorkoutNameChange}
+            />
+          </div>
+
+          {customWorkoutExercises.map(({ exercise, sets }, i) => (
+            <li key={exercise._id}>
+              <p>
+                <span>{i + 1}.</span> {exercise.name}
+              </p>
+
+              {sets.map(({ reps }, j) => (
+                <div key={j}>
+                  <span>set {j + 1}.</span>
+                  <input
+                    type="number"
+                    name="reps"
+                    id="reps"
+                    value={reps}
+                    onChange={(e) => handleRepChange(e, exercise._id, j)}
+                  />{" "}
+                  <span>reps</span>
+                </div>
+              ))}
+
+              <div>
+                <button onClick={() => changeSetLength("add", i)}>Add set</button>
+                <button onClick={() => changeSetLength("remove", i)}>Remove set</button>
+              </div>
+
+              <button onClick={() => removeExercise(exercise)}>Remove</button>
+            </li>
+          ))}
+        </CustomWorkout>
+
+        <ExerciseList>
+          <div className="exercise-control">
+            <div>
+              <label htmlFor="muscleGroup">Muscle Group: </label>
+              <select
+                name="muscleGroup"
+                id="muscleGroup"
+                onChange={(e) => filterExercisesBy({ field: "muscleGroup", value: e.target.value })}
+              >
+                {muscleGroups.map((group) => (
+                  <option key={group} value={group}>
+                    {group}
+                  </option>
+                ))}
+              </select>
+            </div>
+
+            <div>
+              <label htmlFor="motion">Motion: </label>
+              <select
+                name="motion"
+                id="motion"
+                onChange={(e) => filterExercisesBy({ field: "motion", value: e.target.value })}
+              >
+                {motions.map((motion) => (
+                  <option key={motion} value={motion}>
+                    {motion}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+
+          {displayedExercises.map((each) => (
+            <li
+              key={each._id}
+              style={isExerciseInCustomWorkout(each._id) ? { background: "#dddddd" } : {}}
+            >
+              <h3>{each.name}</h3>
+              <p>
+                <span>motion:</span>
+                {each.motion}
+              </p>
+
+              <p>
+                <span>muscle group:</span> {each.muscleGroup}
+              </p>
+              <p>
+                <span>muscle worked:</span> {each.muscleWorked}
+              </p>
+              <p>
+                <span>equipment:</span> {each.equipment}
+              </p>
+              <button onClick={() => addExercise(each)}>Add</button>
+            </li>
+          ))}
+        </ExerciseList>
+
+        <UserWorkouts>
+          <h3>Your Workouts</h3>
+          {userWorkouts.map((each, i) => (
+            <li key={i} onClick={() => displaySavedWorkout(each)}>
+              {each.name}
+            </li>
+          ))}
+        </UserWorkouts>
+      </Container>
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/exercises`);
+  const exercises = await res.json();
+
+  return {
+    props: { exercises },
+  };
+}
+
+/**
+ *
+ *
+ *
+ *
+ * START CSS
+ *
+ *
+ *
+ *
+ *
+ */
+const Container = styled.div`
+  height: 90vh;
+  display: flex;
+  align-items: flex-start;
+  gap: 1rem;
+  position: relative;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    height: fit-content;
+  }
+`;
+
+const CustomWorkout = styled.ul`
+  border: 1px solid black;
+  width: 25%;
+  max-height: 100%;
+  overflow-x: hidden;
+  overflow-y: scroll;
+  position: relative;
+
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  flex-wrap: wrap;
+
+  .workout-control {
+    text-align: center;
+    width: 100%;
+    margin: 0.5rem 0;
+    .workout-header {
+      margin-bottom: 1rem;
+      button {
+        margin: 0 0.5rem;
+        padding: 0.5rem;
+      }
+    }
+    input {
+      width: 50%;
+    }
+  }
+
+  li {
+    border: 1px solid grey;
+    background: rgb(215, 221, 247);
+    width: 100%;
+    max-width: 150px;
+    margin: 0.5rem;
+    text-align: center;
+    text-transform: capitalize;
+
+    display: flex;
+    flex-direction: column;
+    input {
+      width: 3rem;
+    }
+    span {
+      font-weight: 300;
+      font-size: 0.7rem;
+    }
+    button {
+      margin-top: 0.5rem;
+      cursor: pointer;
+    }
+  }
+
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+`;
+
+const ExerciseList = styled.ul`
+  border: 1px solid black;
+  width: 60%;
+  max-height: 100%;
+  overflow-x: hidden;
+  overflow-y: scroll;
+  position: relative;
+
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  flex-wrap: wrap;
+
+  .exercise-control {
+    position: sticky;
+    top: 0;
+    background: #e2e2e2;
+    width: 100%;
+    border-bottom: 1px solid black;
+    text-align: center;
+    div {
+      width: 50%;
+      display: inline-block;
+    }
+  }
+
+  li {
+    border: 1px solid grey;
+    width: 100%;
+    max-width: 150px;
+    margin: 1rem;
+    text-align: center;
+    text-transform: capitalize;
+
+    display: flex;
+    flex-direction: column;
+
+    p {
+      display: block;
+      width: 100%;
+      background: #d9f0f0;
+      border-bottom: 1px solid #d3d3d3;
+      text-align: left;
+      font-weight: 300;
+      span {
+        font-weight: 100;
+        display: block;
+        text-transform: uppercase;
+        font-size: 0.6rem;
+        width: 100%;
+      }
+    }
+
+    button {
+      cursor: pointer;
+    }
+  }
+
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+`;
+
+const UserWorkouts = styled.ul`
+  border: 1px solid black;
+  width: 15%;
+
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  flex-wrap: wrap;
+
+  li {
+    cursor: pointer;
+    border: 1px solid grey;
+    padding: 1rem;
+    width: 100%;
+    margin: 1rem;
+    text-align: center;
+    text-transform: capitalize;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+
+    &:hover {
+      background: grey;
+    }
+  }
+
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+`;
---
+++ pages/workoutLog.js
@@ -0,0 +1,435 @@
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+
+import { formatTime } from "../utils/format";
+import { useStoreContext } from "../context/state";
+import Layout from "../components/Layout";
+import {
+  getExercisesFromIdArray,
+  getUserMadeWorkouts,
+  getWorkoutFromId,
+  saveWorkoutLog,
+} from "../utils/ApiSupply";
+
+export default function workoutLog() {
+  const { user, setUserState } = useStoreContext();
+
+  const [timer, setTimer] = useState(0);
+  const [timerState, setTimerState] = useState("reset"); // 'reset' || 'paused' || 'started'
+  const [currentDayData, setCurrentDayData] = useState({}); // {isoDate, timeInSeconds, completed, workout_id}
+  const [workout, setWorkout] = useState({}); // {exercises[], exercise_id, sets[]}
+  const [yearMonthDay, setYearMonthDay] = useState({}); // {year, month, day}
+  const [userMadeWorkouts, setUserMadeWorkouts] = useState([]);
+
+  // Accepts a workout from user's workoutLog and sets workout and currentDay data
+  const setPageState = async (dayData) => {
+    // Check if workout exists
+    if (dayData) {
+      const workoutData = await getWorkoutFromDB(dayData.workout_id);
+
+      workoutData.exercises.map((each, i) => (each.sets = dayData.exerciseData[i].sets));
+      console.log(workoutData);
+
+      setWorkout(workoutData);
+      setCurrentDayData(dayData);
+    } else {
+      setWorkout({});
+      setCurrentDayData({});
+    }
+  };
+
+  // Accepts an ISO date and finds the matching date in user.workoutLog
+  const getDayDataFromWorkoutLog = (targetIsoDate) => {
+    const dayData = user.workoutLog.find((item) => item.isoDate === targetIsoDate);
+    return dayData;
+  };
+
+  // Change to tomorrow's or yesterday's workout data
+  const changeCurrentDayData = (direction) => {
+    const { year, month, day } = yearMonthDay;
+    const date = new Date(year, month, day);
+
+    switch (direction) {
+      case "tomorrow":
+        date.setDate(date.getDate() + 1);
+        break;
+      case "yesterday":
+        date.setDate(date.getDate() - 1);
+        break;
+    }
+
+    const newYear = date.getFullYear();
+    const newMonth = date.getMonth();
+    const newDay = date.getDate();
+    const newDate = new Date(newYear, newMonth, newDay).toISOString();
+
+    setYearMonthDay({ year: newYear, month: newMonth, day: newDay });
+
+    // Find the workout for the new date
+    const dayData = getDayDataFromWorkoutLog(newDate);
+    setPageState(dayData);
+  };
+
+  // Posts currentDayData to DB
+  const saveWorkout = async () => {
+    // Make sure there are exercises to save
+    if (workout.exercises) {
+      // console.log(currentDayData, workout, yearMonthDay, timer);
+      const composedExercises = workout.exercises.map((each) => {
+        return { exercise_id: each.exercise_id, sets: each.sets };
+      });
+
+      const workoutIndexInLog = user.workoutLog.findIndex(
+        (workout) => workout.isoDate === currentDayData.isoDate
+      );
+
+      let updatedWorkoutLog;
+
+      if (workoutIndexInLog === -1) {
+        // Create new workoutLog entry
+        const { year, month, day } = yearMonthDay;
+        // Add new workout and sort by isoDate
+        updatedWorkoutLog = [
+          ...user.workoutLog,
+          {
+            isoDate: new Date(year, month, day).toISOString(),
+            timeInSeconds: timer,
+            completed: true,
+            workout_id: workout._id,
+            exerciseData: composedExercises,
+          },
+        ].sort((a, b) => a.isoDate.localeCompare(b.isoDate));
+      } else {
+        // Update existing workout
+        const newLog = [...user.workoutLog];
+        // update the index of current workout
+        newLog[workoutIndexInLog] = {
+          ...currentDayData,
+          exerciseData: composedExercises,
+          workout_id: workout._id,
+        };
+
+        updatedWorkoutLog = newLog;
+      }
+
+      const userData = await saveWorkoutLog(updatedWorkoutLog, user._id);
+      setUserState(userData);
+    }
+  };
+
+  // Sets weight for a specific workout. Takes the event value and exercise name
+  const setWeightForExercise = (e, exercise, setIndex) => {
+    // Cast value to number
+    const num = Number(e.target.value);
+
+    // Copy current exercises
+    const { exercises } = workout;
+
+    // Find the exercise being updated
+    exercises.map((item) => {
+      if (item.exercise_id === exercise._id) {
+        // set weight for specified set
+        item.sets[setIndex].weight = num;
+      }
+      return item;
+    });
+
+    // update workout state
+    setWorkout({ ...workout, exercises });
+  };
+
+  const getWorkoutFromDB = async (workout_id) => {
+    try {
+      // GET workout data
+      const workoutData = await getWorkoutFromId(workout_id);
+
+      // Get array of exercise ids
+      const exerciseIdArr = workoutData.exercises.map((each) => each.exercise_id);
+
+      // Translate exercise ids into data
+      const exerciseData = await getExercisesFromIdArray(exerciseIdArr);
+
+      // Create {exercise: exerciseData} in workoutData
+      workoutData.exercises.map((each, i) => {
+        // Ensure ids match
+        if (each.exercise_id === exerciseData[i]._id) {
+          each.exercise = exerciseData[i];
+        }
+        return each;
+      });
+
+      return workoutData;
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const displaySavedWorkout = async (workout) => {
+    // Grab all the exercise_ids from the workout
+    const idArr = workout.exercises.map((each) => each.exercise_id);
+    // Query for exercise data using the idArr
+    const exerciseData = await getExercisesFromIdArray(idArr);
+
+    // Create exercise key in each exercise to hold exercise data
+    workout.exercises.map((each, i) => {
+      // Ensure ids match
+      if (each.exercise_id === exerciseData[i]._id) {
+        each.exercise = exerciseData[i];
+      }
+      return each;
+    });
+
+    setWorkout(workout);
+  };
+
+  // Set page state if user is logged in
+  useEffect(() => {
+    const date = new Date();
+    const currYear = date.getFullYear();
+    const currMonth = date.getMonth();
+    const currDay = date.getDate();
+
+    setYearMonthDay({ year: currYear, month: currMonth, day: currDay });
+
+    if (user) {
+      const currIsoDate = new Date(currYear, currMonth, currDay).toISOString();
+
+      // Find the workout for today
+      const dayData = getDayDataFromWorkoutLog(currIsoDate);
+      setPageState(dayData);
+
+      // Get all workouts made by the user
+      const getUserWorkouts = async () => {
+        const userWorkouts = await getUserMadeWorkouts(user._id);
+        setUserMadeWorkouts(userWorkouts);
+      };
+      getUserWorkouts();
+    }
+  }, [user]);
+
+  // Controls for timer state
+  useEffect(() => {
+    let timerInterval;
+
+    switch (timerState) {
+      case "reset":
+        // Reset timer to 0
+        setTimer(0);
+        clearInterval(timerInterval);
+        break;
+      case "paused":
+        // Timer increments by 0
+        timerInterval = setInterval(() => setTimer((prev) => prev + 0), 1000);
+        break;
+      case "started":
+        // Timer increments by 1 every second
+        timerInterval = setInterval(() => setTimer((prev) => prev + 1), 1000);
+        break;
+    }
+
+    return () => clearInterval(timerInterval);
+  }, [timerState]);
+
+  return (
+    <Layout>
+      <MainContainer>
+        <HeaderContainer>
+          <button onClick={() => changeCurrentDayData("yesterday")}>Back 1 day</button>
+          <div>
+            <h1>{workout.name || "No Workout"}</h1>
+            <h5>{`${yearMonthDay.month + 1}/${yearMonthDay.day}/${yearMonthDay.year}`}</h5>
+            <h3>{currentDayData.completed ? "Completed" : "Incomplete"}</h3>
+          </div>
+          <button onClick={() => changeCurrentDayData("tomorrow")}>Forward 1 day</button>
+        </HeaderContainer>
+
+        <TimerContainer>
+          <h3>{formatTime(timer)}</h3>
+          <div>
+            <button onClick={() => setTimerState("started")}>Start Timer</button>
+            <button onClick={() => setTimerState("paused")}>Pause Timer</button>
+            <button onClick={() => setTimerState("reset")}>Reset Timer</button>
+          </div>
+        </TimerContainer>
+
+        <UserMadeWorkouts>
+          <h3>Your Workouts</h3>
+          <ul>
+            {userMadeWorkouts.map((workout) => (
+              <li key={workout._id} onClick={() => displaySavedWorkout(workout)}>
+                <h4>{workout.name}</h4>
+                <p>{workout.exercises.length} exercises</p>
+              </li>
+            ))}
+          </ul>
+        </UserMadeWorkouts>
+
+        <WorkoutList>
+          {workout.exercises?.map(({ exercise, sets }) => (
+            <li className="exercise" key={exercise._id}>
+              <h3 className="exercise-name">{exercise.name}</h3>
+              <p>{exercise.equipment}</p>
+              <ul>
+                {sets.map(({ reps, weight, weightUnit }, i) => (
+                  <li className="set" key={i}>
+                    <p>
+                      <span>{reps}</span> reps
+                    </p>
+
+                    <div>
+                      <label htmlFor="weight">weight: </label>
+                      <input
+                        type="number"
+                        name="weight"
+                        id="weight"
+                        value={weight || ""}
+                        onChange={(e) => setWeightForExercise(e, exercise, i)}
+                      />
+                      <select name="unit" id="unit" defaultValue={weightUnit}>
+                        <option value="lbs">lbs</option>
+                        <option value="pin">pin</option>
+                      </select>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </li>
+          ))}
+        </WorkoutList>
+
+        <CompleteButton onClick={saveWorkout}>Complete Workout</CompleteButton>
+      </MainContainer>
+    </Layout>
+  );
+}
+
+/**
+ *
+ *
+ *
+ *
+ * START CSS
+ *
+ *
+ *
+ *
+ *
+ */
+const MainContainer = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 1rem;
+`;
+
+const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+
+  div {
+    margin: 0 1rem;
+    h1 {
+      text-transform: uppercase;
+    }
+  }
+`;
+
+const TimerContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  border: 1px solid grey;
+  padding: 1rem;
+  width: 100%;
+  max-width: 325px;
+  margin: 2rem auto;
+  button {
+    margin: 0 0.5rem;
+  }
+`;
+
+const WorkoutList = styled.ul`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  flex-wrap: wrap;
+
+  .exercise {
+    border: 1px solid grey;
+    padding: 0.5rem;
+    max-width: 100%;
+
+    margin: 1rem;
+    text-align: center;
+
+    > h3,
+    > p {
+      text-transform: uppercase;
+    }
+    ul {
+      width: fit-content;
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: center;
+
+      .set {
+        padding: 1rem 0;
+        width: 250px;
+
+        p {
+          padding-bottom: 0.5rem;
+          span {
+            font-weight: 700;
+            font-size: 2rem;
+          }
+        }
+      }
+      input {
+        width: 3rem;
+        font-size: 1.2rem;
+      }
+    }
+  }
+`;
+
+const CompleteButton = styled.button`
+  margin: 2rem auto;
+  font-size: 1.5rem;
+  padding: 0.5rem;
+`;
+
+const UserMadeWorkouts = styled.div`
+  border: 1px solid grey;
+  max-width: 100%;
+  text-align: center;
+  margin-bottom: 2rem;
+
+  ul {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+
+    li {
+      cursor: pointer;
+      margin: 0.5rem;
+      padding: 0.5rem;
+      border: 1px solid grey;
+      &:hover {
+        background: #ccc;
+      }
+
+      h4 {
+        text-transform: capitalize;
+        padding-bottom: 0.5rem;
+      }
+    }
+  }
+`;
