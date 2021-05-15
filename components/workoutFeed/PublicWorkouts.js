cc61fda7328e9fba9a62690a2676eed9b193b259
ChristianAnagnostou <69920522+ChristianAnagnostou@users.noreply.github.com>
2021-05-15 09:41:05 -0700

    add searching in public workouts

--- components/workoutFeed/PublicWorkouts.js
+++ components/workoutFeed/PublicWorkouts.js
@@ -1,6 +1,8 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 
 import PublicWorkoutTile from "./PublicWorkoutTile";
+import SearchIcon from "./SearchIcon";
 
 export default function PublicWorkouts({
   workouts,
@@ -8,11 +10,38 @@ export default function PublicWorkouts({
   removeFromSavedWorkouts,
   addToSavedWorkouts,
 }) {
+  const [filteredWorkouts, setFilteredWorkouts] = useState([]);
+  const [filter, setFilter] = useState("");
+
+  const handleFilterChange = (e) => {
+    setFilter(e.target.value);
+  };
+
+  useEffect(() => {
+    if (workouts.length) {
+      const lowercasedFilter = filter.toLowerCase();
+
+      const filteredData = workouts.filter((workout) => {
+        return workout.name.toLowerCase().includes(lowercasedFilter);
+      });
+
+      setFilteredWorkouts(filteredData);
+    }
+  }, [filter, workouts]);
+
   return (
     <WorkoutList>
-      <p>Tip: Want to try out a workout? Click the + icon to add it to your saved workouts.</p>
+      <div className="searchContainer">
+        <div className="searchBar">
+          <SearchIcon />
+          <input type="text" name="filter" onChange={handleFilterChange} value={filter} />
+        </div>
+        <button onClick={() => setFilter("")}>Cancel</button>
+      </div>
 
-      {workouts.map((workout) => (
+      <h3 className="listTitle">Public Workouts</h3>
+
+      {filteredWorkouts.map((workout) => (
         <PublicWorkoutTile
           key={`public${workout._id}`}
           workout={workout}
@@ -28,7 +57,49 @@ export default function PublicWorkouts({
 const WorkoutList = styled.ul`
   width: 100%;
 
-  p {
-    margin-top: 0.5rem;
+  .searchContainer {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    color: #575757;
+    fill: #575757;
+    font-size: 1.2rem;
+
+    .searchBar {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      background: #eee;
+      width: fit-content;
+      margin: 0.5rem;
+      padding: 0 1rem;
+      border-radius: 10px;
+
+      input {
+        border: none;
+        outline: none;
+        padding: 0.5rem 0 0.5rem 0.5rem;
+        font-size: inherit;
+        background: inherit;
+        color: inherit;
+      }
+    }
+
+    button {
+      border: none;
+      padding: 0.5rem;
+      font-size: inherit;
+      background: inherit;
+      color: inherit;
+    }
+  }
+
+  .listTitle {
+    margin-left: 1.5rem;
+    text-align: left;
+    color: #ccc;
+    font-weight: 400;
   }
 `;
