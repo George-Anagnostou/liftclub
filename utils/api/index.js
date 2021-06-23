3fa201da5d9ac04f02af6a30e7ca342db5f18608
ChristianAnagnostou <69920522+ChristianAnagnostou@users.noreply.github.com>
2021-06-23 09:22:37 -0700

    Move all non-dispatching functions to API folder

--- utils/api/index.js
+++ utils/api/index.js
@@ -18,12 +18,36 @@ export const getUserFromUsername = async (username) => {
   }
 };
 
-/**
- *
- * @param {array} savedWorkouts
- * @param {ObjectId} user_id
- * @returns boolean for save success
- */
+export const joinTeam = async (user_id, team_id) => {
+  try {
+    const res = await fetch(`/api/users/${user_id}`, {
+      method: "PUT",
+      contentType: "application/json",
+      body: JSON.stringify({ joinTeam: team_id }),
+    });
+
+    return true;
+  } catch (e) {
+    console.log(e);
+    return false;
+  }
+};
+
+export const leaveTeam = async (user_id, team_id) => {
+  try {
+    const res = await fetch(`/api/users/${user_id}`, {
+      method: "PUT",
+      contentType: "application/json",
+      body: JSON.stringify({ leaveTeam: team_id }),
+    });
+
+    return true;
+  } catch (e) {
+    console.log(e);
+    return false;
+  }
+};
+
 export const saveSavedWorkouts = async (savedWorkouts, user_id) => {
   try {
     const res = await fetch(`/api/users/${user_id}`, {
@@ -32,11 +56,121 @@ export const saveSavedWorkouts = async (savedWorkouts, user_id) => {
       body: JSON.stringify({ savedWorkouts }),
     });
 
-    // const userData = await res.json();
     return true;
   } catch (e) {
     console.log(e);
     return false;
+  }
+};
+
+export const saveWorkoutLog = async (workoutLog, user_id) => {
+  try {
+    const res = await fetch(`/api/users/${user_id}`, {
+      method: "PUT",
+      contentType: "application/json",
+      body: JSON.stringify({ workoutLog }),
+    });
+
+    const userData = await res.json();
+    return userData;
+  } catch (e) {
+    console.log(e);
+    return false;
+  }
+};
+
+export const saveWeight = async (weight, user_id) => {
+  try {
+    const res = await fetch(`/api/users/${user_id}`, {
+      method: "PUT",
+      contentType: "application/json",
+      body: JSON.stringify({ weight }),
+    });
+
+    const userData = await res.json();
+    return true;
+  } catch (e) {
+    console.log(e);
+    return false;
+  }
+};
+
+export const addFollow = async (user_id, followee_id) => {
+  try {
+    const res = await fetch(`/api/users/${user_id}`, {
+      method: "PUT",
+      contentType: "application/json",
+      body: JSON.stringify({ follow: followee_id }),
+    });
+
+    return true;
+  } catch (e) {
+    console.log(e);
+    return false;
+  }
+};
+
+export const removeFollow = async (user_id, followee_id) => {
+  try {
+    const res = await fetch(`/api/users/${user_id}`, {
+      method: "PUT",
+      contentType: "application/json",
+      body: JSON.stringify({ unfollow: followee_id }),
+    });
+
+    return true;
+  } catch (e) {
+    console.log(e);
+    return false;
+  }
+};
+
+export const saveBio = async (user_id, bio) => {
+  try {
+    const res = await fetch(`/api/users/${user_id}`, {
+      method: "PUT",
+      contentType: "application/json",
+      body: JSON.stringify({ bio }),
+    });
+
+    return true;
+  } catch (e) {
+    console.log(e);
+    return false;
+  }
+};
+
+export const fetchUserSavedWorkouts = async (user_id) => {
+  try {
+    const res = await fetch(`/api/users/${user_id}?field=savedWorkouts`);
+    const workouts = await res.json();
+
+    return workouts;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const fetchDateFromUserWorkoutLog = async (user_id, isoDate) => {
+  try {
+    const res = await fetch(`/api/users/${user_id}?field=workoutLog&date=${isoDate}`);
+    const logData = await res.json();
+
+    return logData;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const deleteWorkoutFromWorkoutLog = async (user_id, isoDate) => {
+  try {
+    const res = await fetch(`/api/users/${user_id}?field=workoutLog&date=${isoDate}`, {
+      method: "DELETE",
+    });
+    const updatedLog = await res.json();
+    return updatedLog;
+  } catch (e) {
+    console.log(e);
   }
 };
 
@@ -194,5 +328,48 @@ export const getWorkoutsFromIdArray = async (idArr) => {
     return workouts;
   } catch (e) {
     console.log(e);
+  }
+};
+
+/*
+ *
+ *
+ *
+ * ROUTINES
+ *
+ *
+ *
+ */
+
+/*
+ *
+ *
+ *
+ * TEAMS
+ *
+ *
+ *
+ */
+export const getTeamById = async (team_id) => {
+  try {
+    const res = await fetch(`/api/teams/${team_id}`);
+    
+    const team = await res.json();
+    return team;
+  } catch (e) {
+    console.log(e);
+    return false;
+  }
+};
+
+export const getUserMadeTeams = async (user_id) => {
+  try {
+    const res = await fetch(`/api/teams?creator_id=${user_id}`);
+    const teams = await res.json();
+
+    return teams;
+  } catch (e) {
+    console.log(e);
+    return false;
   }
 };
