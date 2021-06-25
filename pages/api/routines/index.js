e07f21d03bd21979aae52ee20695afabc4026292
ChristianAnagnostou <69920522+ChristianAnagnostou@users.noreply.github.com>
2021-06-25 13:03:20 -0700

    POST handler for newRoutine

--- pages/api/routines/index.js
+++ pages/api/routines/index.js
@@ -1,4 +1,5 @@
 import { connectToDatabase } from "../../../utils/mongodb";
+import { ObjectId } from "mongodb";
 
 export default async (req, res) => {
   const httpMethod = req.method;
@@ -10,15 +11,20 @@ export default async (req, res) => {
       res.json(routines);
       break;
     case "POST":
-      const routine = JSON.parse(req.body);
+      const { newRoutine } = JSON.parse(req.body);
 
-      console.log(routine);
+      console.log(newRoutine);
 
-      // const added = await db.collection("routines").insertOne(routine);
+      newRoutine.workoutPlan.map((entry) => {
+        entry.workout_id = ObjectId(entry.workout_id);
+        entry.isoDate = new Date(entry.isoDate);
+      });
+      newRoutine.creator_id = ObjectId(newRoutine.creator_id);
+      newRoutine._id = ObjectId(newRoutine._id);
 
-      // added.insertedId ? res.status(201).end() : res.status(404).end();
+      const added = await db.collection("routines").insertOne(newRoutine);
 
-      res.json({});
+      added.insertedId ? res.status(201).end() : res.status(404).end();
 
       break;
     case "PUT":
