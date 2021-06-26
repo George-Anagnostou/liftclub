82ab1f0f06a325495707dcb12dd346f0eb4bb154
ChristianAnagnostou <69920522+ChristianAnagnostou@users.noreply.github.com>
2021-06-26 09:53:55 -0700

    Add: meta tag for apple mobile web-app capable

--- components/SeoHead.js
+++ components/SeoHead.js
@@ -6,6 +6,7 @@ const SeoHead = ({ title }) => {
       <title>{title}</title>
       <link rel="icon" href="/favicon.jpeg" />
       <link rel="apple-touch-icon" href="/favicon.jpeg" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
     </Head>
   );
 };
