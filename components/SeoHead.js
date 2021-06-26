5db77cfbe6c72449227922f67b0a761095d37b59
ChristianAnagnostou <69920522+ChristianAnagnostou@users.noreply.github.com>
2021-06-26 10:18:13 -0700

    SEO edits and add manifest.json

--- components/SeoHead.js
+++ components/SeoHead.js
@@ -6,7 +6,10 @@ const SeoHead = ({ title }) => {
       <title>{title}</title>
       <link rel="icon" href="/favicon.jpeg" />
       <link rel="apple-touch-icon" href="/favicon.jpeg" />
+      <meta name="viewport" content="width=device-width,initial-scale=1" />
       <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="mobile-web-app-capable" content="yes" />
+      <link rel="manifest" href="/manifest.json" />
     </Head>
   );
 };
