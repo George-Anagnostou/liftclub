78351faa7945586d0c53d8d52f9b849e6001f243
Christian Anagnostou <christian0722@gmail.com>
2021-07-13 00:19:12 -0700

    Create interfaces for entire app

---
+++ utils/interfaces.ts
@@ -0,0 +1,70 @@
+export interface User {
+  _id: string;
+  username: string;
+  savedWorkouts?: Array<string>;
+  workoutLog: WorkoutLogItem[];
+  weight?: Array<number>;
+  following?: Array<string>;
+  followers?: Array<string>;
+  bio?: string;
+  isTrainer?: boolean;
+  teamsJoined?: Array<string>;
+  settings?: { showBio: boolean };
+  profileImg?: number;
+}
+
+export interface Workout {
+  _id: string;
+  creator_id: string;
+  creatorName: string;
+  name: string;
+  exercises: {
+    exercise_id: string;
+    sets: { reps: number; weight: number }[];
+  }[];
+  isPublic: boolean;
+  date_created: string;
+}
+
+export type WorkoutLog = WorkoutLogItem[];
+
+export interface WorkoutLogItem {
+  isoDate: string;
+  completed: boolean;
+  workoutName: string;
+  workout_id: string;
+  workoutNote: string;
+  exerciseData: {
+    exercise_id: string;
+    sets: { reps: number; weight: number | string }[];
+  }[];
+}
+
+export interface Team {
+  _id: string;
+  teamName: string;
+  members: Array<string>;
+  dateCreated: string;
+  creatorName: string;
+  creator_id: string;
+  trainers: Array<string>;
+  routine_id: string;
+  routineName?: string;
+  routine?: Routine;
+}
+
+export interface Routine {
+  _id: string;
+  creator_id: string;
+  creatorName: string;
+  name: string;
+  workoutPlan: { isoDate: string; workout_id: string }[];
+}
+
+export interface Exercise {
+  _id: string;
+  name: string;
+  equipment: string;
+  muscleGroup: string;
+  muscleWorked: string;
+}
