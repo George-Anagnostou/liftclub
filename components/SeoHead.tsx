22ee70c1c5eca4dff7fab52895b997c7ad090bb9
ChristianAnagnostou <69920522+ChristianAnagnostou@users.noreply.github.com>
2021-07-20 23:27:16 -0700

    apple touch icon to jpeg

--- components/SeoHead.tsx
+++ components/SeoHead.tsx
@@ -9,7 +9,7 @@ const SeoHead: React.FC<Props> = ({ title }) => {
     <Head>
       <title>{title}</title>
       <link rel="icon" href="/favicon.png" />
-      <link rel="apple-touch-icon" href="/favicon.png" />
+      <link rel="apple-touch-icon" href="/favicon.jpeg" />
       <meta name="viewport" content="width=device-width,initial-scale=1" />
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="mobile-web-app-capable" content="yes" />
