54492c8043bf2a8b0ae1e9eed5cf34ae7b3a5326
ChristianAnagnostou <69920522+ChristianAnagnostou@users.noreply.github.com>
2021-07-23 09:10:37 -0700

    add: poppins global font

--- components/GlobalStyles.js
+++ components/GlobalStyles.js
@@ -12,7 +12,7 @@ export const GlobalStyles = createGlobalStyle`
   body {
     background: ${({ theme }) => theme.body};
     color: ${({ theme }) => theme.text};
-    font-family: Tahoma, Helvetica, Arial, Roboto, sans-serif;
+    font-family: Poppins ,Tahoma, Helvetica, Arial, Roboto, sans-serif;
     transition: all 0.25s linear;
     scroll-behavior: smooth
   }
--- components/SeoHead.tsx
+++ components/SeoHead.tsx
@@ -8,12 +8,20 @@ const SeoHead: React.FC<Props> = ({ title }) => {
   return (
     <Head>
       <title>{title}</title>
+
       <link rel="icon" href="/favicon.png" />
       <link rel="apple-touch-icon" href="/favicon.jpeg" />
       <meta name="viewport" content="width=device-width,initial-scale=1" />
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="mobile-web-app-capable" content="yes" />
       <link rel="manifest" href="/manifest.json" />
+
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,900;1,400&display=swap"
+        rel="stylesheet"
+      />
     </Head>
   );
 };
