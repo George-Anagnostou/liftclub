697a9afc95ae9744811351dbc8cdf50517fb6743
Christian Anagnostou <christian0722@gmail.com>
2021-08-05 13:00:21 -0700

    add: ServiceWorker for notifications

--- components/SeoHead.tsx
+++ components/SeoHead.tsx
@@ -12,8 +12,11 @@ const SeoHead: React.FC<Props> = ({ title }) => {
       <link rel="icon" href="/favicon.png" />
       <link rel="apple-touch-icon" href="/favicon.jpeg" />
       <meta name="viewport" content="width=device-width,initial-scale=1" />
+
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="mobile-web-app-capable" content="yes" />
+
+      <link rel="serviceworker" href="/sw.js" />
       <link rel="manifest" href="/manifest.json" />
 
       <link rel="preconnect" href="https://fonts.googleapis.com" />
