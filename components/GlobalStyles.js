94359dd83d74c4fa0bdf1cf853517482c3d908ef
ChristianAnagnostou <69920522+ChristianAnagnostou@users.noreply.github.com>
2021-10-28 21:24:55 -0700

    minimal custom scrollbar

--- components/GlobalStyles.js
+++ components/GlobalStyles.js
@@ -35,4 +35,23 @@ export const GlobalStyles = createGlobalStyle`
   ul {
     list-style: none;
   }
+
+  ::-webkit-scrollbar {
+    width: 20px;
+  }
+
+  ::-webkit-scrollbar-track {
+    background-color: transparent;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background-color: #818181;
+    border-radius: 20px;
+    border: 6px solid transparent;
+    background-clip: content-box;
+  }
+  
+  ::-webkit-scrollbar-thumb:hover {
+    background-color: #a7a7a7;
+  }
 `;
