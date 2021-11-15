69472a45044e776d27831ca67b57466f694da449
ChristianAnagnostou <69920522+ChristianAnagnostou@users.noreply.github.com>
2021-11-14 17:32:10 -0800

    create useInViewEffect hook

---
+++ components/hooks/useInViewEffect.ts
@@ -0,0 +1,44 @@
+import { useRef, useCallback } from "react";
+
+interface UseInViewEffect {
+  (
+    callback: IntersectionObserverCallback,
+    options?: IntersectionObserverInit,
+    externalState?: React.ComponentState[]
+  ): (node: Element | null) => void;
+}
+
+/**
+ * useInViewEffect
+ * @param callback IntersectionObserverCallback
+ * @param options IntersectionObserverInit
+ * @param externalState React.ComponentState[]
+ */
+const useInViewEffect: UseInViewEffect = (
+  callback,
+  { root, rootMargin, threshold } = {},
+  externalState = []
+) => {
+  const target = useRef<Element | null>(null);
+  const observer = useRef<IntersectionObserver | null>(null);
+
+  const setTarget = useCallback((node) => {
+      if (target.current && observer.current) {
+        observer.current.unobserve(target.current);
+        observer.current.disconnect();
+        observer.current = null;
+      }
+
+      if (node) {
+        observer.current = new IntersectionObserver(callback, { root, rootMargin, threshold });
+        observer.current.observe(node);
+        target.current = node;
+      }
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [target, root, rootMargin, threshold, ...externalState]
+  );
+
+  return setTarget;
+};
+
+export default useInViewEffect;
