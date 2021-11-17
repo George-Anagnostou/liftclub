824d42cc523b2c73c226b66ec9088678d13cfc12
ChristianAnagnostou <69920522+ChristianAnagnostou@users.noreply.github.com>
2021-11-16 19:39:14 -0800

    create custom middleware for handling auth token validation

---
+++ api-lib/auth/middleware.ts
@@ -0,0 +1,27 @@
+import { NextApiRequest } from "next";
+import * as jwt from "jsonwebtoken";
+import { ObjectId } from "mongodb";
+
+export function verifyAuthToken(req: NextApiRequest, match?: string | ObjectId) {
+  if (match instanceof ObjectId) {
+    match = match.toString();
+  }
+
+  const token = req.headers.token as string;
+  const JWT_SECRET = process.env.JWT_SECRET || "";
+  
+  try {
+    if (!JWT_SECRET || !token) throw new Error("A system error has occured.");
+
+    const verified = jwt.verify(token, JWT_SECRET) as { id: string; iat: number };
+
+    if (match && match !== verified.id) {
+      throw new Error("Invalid credentials for specified request");
+    }
+
+    return verified.id;
+  } catch (e) {
+    console.log(e);
+    return false;
+  }
+}
