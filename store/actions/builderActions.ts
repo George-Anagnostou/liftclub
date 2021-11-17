5f86e39e02ce092a5ea44b0b711265d5f47f44f2
ChristianAnagnostou <69920522+ChristianAnagnostou@users.noreply.github.com>
2021-11-17 14:43:00 -0800

    clean up spacing for builderActions

--- store/actions/builderActions.ts
+++ store/actions/builderActions.ts
@@ -1,4 +1,3 @@
-import { EditableTeam } from "../../components/builder/team";
 import {
   getUserMadeWorkouts,
   getWorkoutsFromIdArray,
@@ -14,19 +13,18 @@ import {
   postNewTeam,
   updateTeam,
 } from "../../api-lib/fetchers";
-import { NewRoutine, NewWorkout, Routine, User, Workout } from "../../types/interfaces";
+import { NewTeam, NewWorkout, Routine, Team, User, Workout } from "../../types/interfaces";
+
+// Workouts
 
 export const getUserCreatedWorkouts = async (dispatch, user_id: string) => {
   const workouts = await getUserMadeWorkouts(user_id);
-
   dispatch({ type: "UPDATE_ALL_CREATED_WORKOUTS", payload: { workouts } });
 };
 
 export const getUserSavedWorkouts = async (dispatch, idArr: string[]) => {
   if (idArr.length === 0) return;
-
   const workouts = await getWorkoutsFromIdArray(idArr);
-
   dispatch({ type: "UPDATE_ALL_SAVED_WORKOUTS", payload: { workouts } });
 };
 
@@ -35,9 +33,7 @@ export const addWorkoutToCreatedWorkouts = async (dispatch, workout: NewWorkout)
 
   if (workout_id) {
     workout["_id"] = workout_id;
-
     dispatch({ type: "ADD_CREATED_WORKOUT", payload: { workout } });
-
     return true;
   } else {
     return false;
@@ -46,19 +42,17 @@ export const addWorkoutToCreatedWorkouts = async (dispatch, workout: NewWorkout)
 
 export const deleteWorkoutFromCreatedWorkouts = async (dispatch, workout_id: string) => {
   const deleted = await deleteWorkout(workout_id);
-
   if (deleted) dispatch({ type: "DELETE_CREATED_WORKOUT", payload: { workout_id } });
-
   return deleted;
 };
 
 export const updateExistingCreatedWorkout = async (dispatch, workout: Workout) => {
   const saveStatus = await updateExistingWorkout(workout);
-
   if (saveStatus) dispatch({ type: "UPDATE_CREATED_WORKOUT", payload: { workout } });
-
   return saveStatus;
 };
+
+// Routines
 
 export const getUserCreatedRoutines = async (dispatch, user_id: string) => {
   const routines = await getRoutinesFromCreatorId(user_id);
@@ -67,34 +61,21 @@ export const getUserCreatedRoutines = async (dispatch, user_id: string) => {
 
 export const deleteRoutineFromCreatedRoutines = async (dispatch, routine_id: string) => {
   const deleted = await deleteRoutine(routine_id);
-
   if (deleted) dispatch({ type: "DELETE_CREATED_ROUTINE", payload: { routine_id } });
-
   return deleted;
 };
 
 export const addRoutineToCreatedRoutines = async (dispatch, routine: Routine, user: User) => {
-  routine.creator_id = user._id;
-  routine.creatorName = user.username;
-  routine.name = routine.name || "New Routine";
+  const { _id, ...newRoutine } = routine;
+  newRoutine.creator_id = user._id;
+  newRoutine.creatorName = user.username;
+  newRoutine.name = routine.name || "New Routine";
 
-  const routineForDB: NewRoutine = {
-    ...routine,
-    workoutPlan: routine.workoutPlan.map(({ isoDate, workout_id }) => ({
-      isoDate,
-      workout_id,
-    })),
-  };
-
-  delete routineForDB["_id"];
-
-  const routine_id = await postNewRoutine(routineForDB);
+  const routine_id = await postNewRoutine(newRoutine);
 
   if (routine_id) {
-    routine._id = routine_id;
-
-    dispatch({ type: "ADD_CREATED_ROUTINE", payload: { routine: routine } });
-
+    newRoutine["_id"] = routine_id;
+    dispatch({ type: "ADD_CREATED_ROUTINE", payload: { routine: newRoutine } });
     return true;
   } else {
     return false;
@@ -102,20 +83,12 @@ export const addRoutineToCreatedRoutines = async (dispatch, routine: Routine, us
 };
 
 export const updateExistingCreatedRoutine = async (dispatch, routine: Routine) => {
-  const routineForDB: any = {
-    ...routine,
-    workoutPlan: routine.workoutPlan.map(({ isoDate, workout_id }) => ({
-      isoDate,
-      workout_id,
-    })),
-  };
-
-  const saveStatus = await updateRoutine(routineForDB);
-
+  const saveStatus = await updateRoutine(routine);
   if (saveStatus) dispatch({ type: "UPDATE_CREATED_ROUTINE", payload: { routine } });
-
   return saveStatus;
 };
+
+// Teams
 
 export const getUserCreatedTeams = async (dispatch, user_id: string) => {
   const teams = await getUserMadeTeams(user_id);
@@ -124,28 +97,22 @@ export const getUserCreatedTeams = async (dispatch, user_id: string) => {
 
 export const deleteTeamFromCreatedTeams = async (dispatch, team_id: string) => {
   const deleted = await deleteTeam(team_id);
-
   if (deleted) dispatch({ type: "DELETE_CREATED_TEAM", payload: { team_id } });
-
   return deleted;
 };
 
-export const addTeamToCreatedTeams = async (dispatch, newTeam: EditableTeam) => {
+export const addTeamToCreatedTeams = async (dispatch, newTeam: NewTeam) => {
   const team_id = await postNewTeam(newTeam);
 
   if (team_id) {
-    newTeam._id = team_id;
-
+    newTeam["_id"] = team_id;
     dispatch({ type: "ADD_CREATED_TEAM", payload: { team: newTeam } });
   }
-
   return team_id;
 };
 
-export const updateExistingCreatedTeam = async (dispatch, team: EditableTeam) => {
+export const updateExistingCreatedTeam = async (dispatch, team: Team) => {
   const saveStatus = await updateTeam(team);
-
   if (saveStatus) dispatch({ type: "UPDATE_CREATED_TEAM", payload: { team } });
-
   return saveStatus;
 };
