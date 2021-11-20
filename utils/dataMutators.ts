523acf533598bbc72c4096271339d4a01838fc67
ChristianAnagnostou <69920522+ChristianAnagnostou@users.noreply.github.com>
2021-11-19 19:21:38 -0800

    fix: sort the response data into the order it was received as

--- utils/dataMutators.ts
+++ utils/dataMutators.ts
@@ -7,35 +7,23 @@ import {
   WorkoutLogItem,
 } from "../types/interfaces";
 
-export const addExerciseDataToLoggedWorkout = async (workout: WorkoutLogItem) => {
+export const addExerciseDataToLoggedWorkout = async (logItem: WorkoutLogItem) => {
   // Grab all exercise_ids from the workout
-  const idArr = workout.exerciseData.map((each) => each.exercise_id);
-
+  const idArr = logItem.exerciseData.map((each) => each.exercise_id);
   // Get all exercise information
   const exerciseData = await getExercisesFromIdArray(idArr);
-
-  // Sort the array based on the order of the idArr
-  exerciseData.sort((a, b) => idArr.indexOf(a._id) - idArr.indexOf(b._id));
-
   // Create exercise key in each exercise to hold exercise data
-  workout.exerciseData.map((each, i) => (each.exercise = exerciseData[i]));
-
-  return workout;
+  logItem.exerciseData.map((each, i) => (each.exercise = exerciseData[i]));
+  return logItem;
 };
 
 export const addExerciseDataToWorkout = async (workout: Workout) => {
   // Grab all the exercise_ids from the workout
   const idArr = workout.exercises.map((each) => each.exercise_id);
-
   // Query for exercise data using the idArr
   const exerciseData = await getExercisesFromIdArray(idArr);
-
-  // Sort the array based on the order of the idArr
-  exerciseData.sort((a, b) => idArr.indexOf(a._id) - idArr.indexOf(b._id));
-
   // Create exercise key in each exercise to hold exercise data
   workout.exercises.map((each, i) => (each.exercise = exerciseData[i]));
-
   return workout;
 };
 
