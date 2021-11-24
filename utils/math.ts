cbc2c0decf39b925d4ec412d142315e7c544871c
ChristianAnagnostou <69920522+ChristianAnagnostou@users.noreply.github.com>
2021-11-24 14:08:47 -0800

    extract formatSetWeight function

--- utils/math.ts
+++ utils/math.ts
@@ -2,3 +2,7 @@ export const round = (value: number, precision: number) => {
   var multiplier = Math.pow(10, precision || 0);
   return Math.round(value * multiplier) / multiplier;
 };
+
+export const formatSetWeight = (weight: number | string) => {
+  return typeof weight === "string" || weight === -1 ? 0 : weight;
+};
