9c2681d050e151f5f55dd54c509dcf1eed08f6a0
ChristianAnagnostou <69920522+ChristianAnagnostou@users.noreply.github.com>
2021-11-24 00:47:29 -0800

    fix: sort the exercise history of groupedWorkouts by date

--- utils/dataMutators.ts
+++ utils/dataMutators.ts
@@ -1,4 +1,5 @@
 import { getExercisesFromIdArray } from "../api-lib/fetchers";
+import { ExerciseHistoryMap } from "../components/userProfile/ProgressTile";
 import {
   Routine,
   RoutineWorkoutPlanForCalendar,
@@ -57,18 +58,25 @@ export const formatRoutineWorkoutPlanForCalendar = (plan: Routine["workoutPlan"]
  * @returns a Map() where the keys are exercise id's and the values are arrays of all exercise sets that have been logged to the exercise and sorted by most recent date
  */
 export const groupWorkoutLogByExercise = (log: WorkoutLog) => {
-  const group: Map<string, { sets: { reps: number; weight: string | number }[]; date: string }[]> =
-    new Map();
+  const group: ExerciseHistoryMap = new Map();
 
   for (let [date, { exerciseData }] of Object.entries(log)) {
     exerciseData.forEach(({ exercise_id, sets }) => {
       const curr = group.get(exercise_id);
       if (curr) {
-        group.set(exercise_id, [{ date: date, sets: sets }, ...curr]);
+        group.set(exercise_id, [{ date: date, sets: sets, exercise_id }, ...curr]);
       } else {
-        group.set(exercise_id, [{ date: date, sets: sets }]);
+        group.set(exercise_id, [{ date: date, sets: sets, exercise_id }]);
       }
     });
+  }
+
+  // Sort all exercise history's by date (newest to oldest)
+  for (let [exercise_id, exerciseHistory] of [...group.entries()]) {
+    group.set(
+      exercise_id,
+      exerciseHistory.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
+    );
   }
 
   return group;
